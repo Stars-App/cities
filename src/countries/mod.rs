9fca78b545bba6dfc139674ef24b66386a54a49e
kalucky0 <kalucki23@gmail.com>
2022-02-16 14:43:23 +0100

    Add Austria

--- src/countries/mod.rs
+++ src/countries/mod.rs
@@ -2,12 +2,14 @@ use crate::processor::Place;
 
 pub mod albania;
 pub mod andorra;
+pub mod austria;
 pub mod poland;
 
 pub fn get_data() -> Vec<Place> {
     let mut data = Vec::new();
     data.extend(albania::PLACES);
     data.extend(andorra::PLACES);
+    data.extend(austria::PLACES);
     data.extend(poland::PLACES);
     data
 }
