793b384217e47e7edc056ad5a71af7dde641cdca
kalucky0 <kalucki23@gmail.com>
2022-02-16 14:33:31 +0100

    Add Andorra

--- src/countries/mod.rs
+++ src/countries/mod.rs
@@ -1,11 +1,13 @@
 use crate::processor::Place;
 
 pub mod albania;
+pub mod andorra;
 pub mod poland;
 
 pub fn get_data() -> Vec<Place> {
     let mut data = Vec::new();
     data.extend(albania::PLACES);
+    data.extend(andorra::PLACES);
     data.extend(poland::PLACES);
     data
 }
